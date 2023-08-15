050ed91cc69c59c9b8625d2f6c5097315e3ab62c
danielbrunobz <danielbrunobz@gmail.com>
2023-08-15 19:19:18 -0300

    RestClient implemented, Application Providers created

---
+++ lib/src/core/providers/application_providers.dart
@@ -0,0 +1,6 @@
+import 'package:dw_barbershop/src/core/restClient/rest_client.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'application_providers.g.dart';
+
+@Riverpod(keepAlive: true)
+RestClient restClient(RestClientRef ref) => RestClient();
