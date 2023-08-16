83b00800c8ac5f1e370cf29ddd9a9df583299631
danielbrunobz <danielbrunobz@gmail.com>
2023-08-16 15:06:10 -0300

    Model

--- lib/src/core/features/auth/login/login_page.dart
+++ lib/src/core/features/auth/login/login_page.dart
@@ -1,18 +1,56 @@
+import 'package:dw_barbershop/src/core/features/auth/login/login_state.dart';
+import 'package:dw_barbershop/src/core/features/auth/login/login_vm.dart';
 import 'package:dw_barbershop/src/core/ui/constants.dart';
+import 'package:dw_barbershop/src/core/ui/helpers/form_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:validatorless/validatorless.dart';
+import '../../../ui/helpers/messages.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({super.key});
 
   @override
+  ConsumerState<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends ConsumerState<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    emailEC.dispose();
+    passwordEC.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final LoginVm(:login) = ref.watch(loginVmProvider.notifier);
+
+    ref.listen(loginVmProvider, (_, state) {
+      switch (state) {
+        case LoginState(status: LoginStateStatus.initial):
+          break;
+        case LoginState(status: LoginStateStatus.error, :final errorMessage?):
+          Messages.showError(errorMessage, context);
+        case LoginState(status: LoginStateStatus.error):
+          Messages.showError('Erro ao realizar login', context);
+          break;
+      }
+    });
+
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: DecoratedBox(
+      backgroundColor: Colors.black,
+      body: Form(
+        key: formKey,
+        child: DecoratedBox(
           decoration: const BoxDecoration(
             image: DecorationImage(
                 image: AssetImage(
-                  'assets/images/background_image_chair.jpg',
+                  ImageConstants.backgroundChair,
                 ),
                 opacity: 0.2,
                 fit: BoxFit.cover),
@@ -27,66 +65,94 @@ class LoginPage extends StatelessWidget {
                     alignment: Alignment.center,
                     children: [
                       Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                      Image.asset('assets/images/imgLogo.png'),
-                      const SizedBox(
-                         height: 30,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          label: Text('E-mail'),
-                          hintText: 'E-mail',
-                          floatingLabelBehavior: FloatingLabelBehavior.never,
-                          labelStyle: TextStyle(color: Colors.black),
-                          hintStyle: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                      const SizedBox(
-                         height: 24,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          label: Text('Senha'),
-                          hintText: 'Senha',
-                          floatingLabelBehavior: FloatingLabelBehavior.never,
-                          labelStyle: TextStyle(color: Colors.black),
-                          hintStyle: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 26,
-                      ),
-                      const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text('Esqueceu a senha?',
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: ColorsConstants.brown,
-                        ),
-                        ),
-                      ),
-                      const SizedBox(
-                         height: 24,
-                      ),
-                      ElevatedButton(   
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size.fromHeight(56),
-                        ),  
-                        onPressed: (){},
-                        child: const Text('ACESSAR',
-                        ),
-                        ),
-                      ],
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(ImageConstants.imageLogo),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          TextFormField(
+                            onTapOutside: (_) => context.unfocus(),
+                            validator: Validatorless.multiple([
+                              Validatorless.required('E-mail obriigatório'),
+                              Validatorless.email('E-mail inválido'),
+                            ]),
+                            controller: emailEC,
+                            decoration: const InputDecoration(
+                              label: Text('E-mail'),
+                              hintText: 'E-email',
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.never,
+                              labelStyle: TextStyle(color: Colors.black),
+                              hintStyle: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          TextFormField(
+                            onTapOutside: (_) => context.unfocus(),
+                            validator: Validatorless.multiple([
+                              Validatorless.required('Senha obrigatória'),
+                              Validatorless.min(6,
+                                  'Senha deve conter pelo menos 6 caracteres'),
+                            ]),
+                            obscureText: true,
+                            controller: passwordEC,
+                            decoration: const InputDecoration(
+                              label: Text('Senha'),
+                              hintText: 'Senha',
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.never,
+                              labelStyle: TextStyle(color: Colors.black),
+                              hintStyle: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Esqueceu a senha?',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: ColorsConstants.brown,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size.fromHeight(56),
+                            ),
+                            onPressed: () {
+                              switch (formKey.currentState?.validate()) {
+                                case (false || null):
+                                  Messages.showError(
+                                      'Campos inválidos', context);
+                                  break;
+                                case true:
+                                  login(emailEC.text, passwordEC.text);
+                              }
+                            },
+                            child: const Text(
+                              'ACESSAR',
+                            ),
+                          ),
+                        ],
                       ),
                       const Align(
                         alignment: Alignment.bottomCenter,
-                        child: Text('Criar conta',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500,
-                        ),
+                        child: Text(
+                          'Criar conta',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
                     ],
@@ -96,6 +162,7 @@ class LoginPage extends StatelessWidget {
             ),
           ),
         ),
-      );
+      ),
+    );
   }
 }
---
+++ lib/src/core/features/auth/login/login_vm.dart
@@ -0,0 +1,34 @@
+import 'package:asyncstate/asyncstate.dart';
+import 'package:dw_barbershop/src/core/fp/either.dart';
+import 'package:dw_barbershop/src/core/providers/application_providers.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../exceptions/service_exception.dart';
+import 'login_state.dart';
+part 'login_vm.g.dart';
+
+@riverpod
+class LoginVm extends _$LoginVm {
+  @override
+  LoginState build() => LoginState.initial();
+
+  Future<void> login(String email, String password) async {
+    final loaderHandle = AsyncLoaderHandler()..start();
+
+    final loginService = ref.watch(userLoginServiceProvider);
+
+    final result = await loginService.execute(email, password);
+
+    switch (result) {
+      case Success():
+        break;
+
+      case Failure(exception: ServiceException(:final message)):
+        state = state.copyWith(
+          status: LoginStateStatus.error,
+          errorMessage: () => message,
+        );
+    }
+    loaderHandle.close();
+  }
+}
