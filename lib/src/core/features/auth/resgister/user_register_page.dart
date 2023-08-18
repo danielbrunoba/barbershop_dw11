e5a6d9fe305d0032425ecb33030a6ab455ed9798
danielbrunobz <danielbrunobz@gmail.com>
2023-08-18 16:08:50 -0300

    user_register

--- lib/src/core/features/auth/resgister/user_register_page.dart
+++ lib/src/core/features/auth/resgister/user_register_page.dart
@@ -1,62 +1,135 @@
+import 'package:dw_barbershop/src/core/ui/helpers/form_helper.dart';
+import 'package:dw_barbershop/src/core/ui/helpers/messages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:validatorless/validatorless.dart';
 
-class UserRegisterPage extends StatelessWidget {
+import 'user_register_vm.dart';
 
-  const UserRegisterPage({ super.key });
+class UserRegisterPage extends ConsumerStatefulWidget {
+  const UserRegisterPage({super.key});
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Criar conta'),),
-           body: Padding(
-             padding: const EdgeInsets.all(30.0),
-             child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('Nome'),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('E-mail'),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('Senha'),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('Confirmar senha'),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(56),
-                    ),
-                    onPressed: () {},
-                  child: const Text('CRIAR CONTA'),),
-                ],
-              )),
-           ),
-       );
+  @override
+  ConsumerState<UserRegisterPage> createState() => _UserRegisterPageState();
+}
+
+class _UserRegisterPageState extends ConsumerState<UserRegisterPage> {
+  final formKey = GlobalKey<FormState>();
+  final nameEC = TextEditingController();
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    nameEC.dispose();
+    emailEC.dispose();
+    passwordEC.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final UserRegisterVm = ref.watch(userRegisterVmProvider.notifier);
+
+    ref.listen(userRegisterVmProvider, (_, state) {
+      switch (state) {
+        case UserRegisterStateStatus.initial:
+        case UserRegisterStateStatus.success:
+        case UserRegisterStateStatus.error:
+      }
+    });
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Criar conta'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: SingleChildScrollView(
+            child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                onTapOutside: (_) => context.unfocus(),
+                controller: nameEC,
+                validator: Validatorless.required('Nome obrigatório'),
+                decoration: const InputDecoration(
+                  label: Text('Nome'),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                onTapOutside: (_) => context.unfocus(),
+                controller: emailEC,
+                validator: Validatorless.multiple([
+                  Validatorless.required('E-mail obrigatório'),
+                  Validatorless.email('E-mail inválido'),
+                ]),
+                decoration: const InputDecoration(
+                  label: Text('E-mail'),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                onTapOutside: (_) => context.unfocus(),
+                controller: passwordEC,
+                validator: Validatorless.multiple([
+                  Validatorless.required('Senha obrigatória'),
+                  Validatorless.min(
+                      6, 'Senha deve conter no mínimo 6 caracteres'),
+                ]),
+                obscureText: true,
+                decoration: const InputDecoration(
+                  label: Text('Senha'),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                onTapOutside: (_) => context.unfocus(),
+                validator: Validatorless.multiple([
+                  Validatorless.required('Confirmar senha é obrigatório'),
+                  Validatorless.compare(passwordEC, 'As senhas não coincidem')
+                ]),
+                obscureText: true,
+                decoration: const InputDecoration(
+                  label: Text('Confirmar senha'),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(56),
+                ),
+                onPressed: () {
+                  switch (formKey.currentState?.validate()) {
+                    case null || false:
+                      Messages.showError('Formulário inválido', context);
+                    case true:
+                      UserRegisterVm.register(
+                        name: nameEC.text,
+                        email: emailEC.text,
+                        password: passwordEC.text,
+                      );
+                  }
+                },
+                child: const Text('CRIAR CONTA'),
+              ),
+            ],
+          ),
+        )),
+      ),
+    );
   }
 }
